76dcaa339e654c225798b4aae985d6985bc90a86
niels <nm@9la.dk>
2016-08-03 19:56:24 +0200

    Replace hello world UI with fundamental connection header and tunnel list.

--- app/containers/App.js
+++ app/containers/App.js
@@ -7,23 +7,54 @@ class App extends Component {
     this.props.dispatch(connectSSH())
   }
 
+  renderConnectionHeader() {
+    let connection = this.props.connection.toJS();
+    return (
+      <div>
+        <p>
+          <strong><a onClick={this.onHelloClick.bind(this)}>Connect</a></strong>
+          <span> {connection.config.username}@{connection.config.host} </span>
+        </p>
+      </div>
+    )
+  }
+
   renderConnectionIndicator() {
     let status = this.props.connection.get('status').toJS();
     if (status.isConnecting) {
       return <p>Connecting...</p>
     }
     if (status.isConnected) {
-      return <p>Yay, Connected!</p>
+      return <p>Connected</p>
     }
+    return <p>Not connected</p>
+  }
+
+  renderTunnelList() {
+    let entries = this.props.tunnels.entries;
+    return (
+      <ul>
+        {
+          entries.map(tunnel => {
+            return (
+              <li key={tunnel.name}>
+                <strong>enable</strong>
+                <span> {tunnel.name}</span>
+              </li>
+            )
+          })
+        }
+      </ul>
+    )
   }
 
   render() {
     console.log(JSON.stringify(this.props))
-    let tunnels = this.props.tunnels
-    let entry = tunnels.entries[0]
     return <div>
-      <p onClick={this.onHelloClick.bind(this)}>Hello {entry.name}!</p>
+      {this.renderConnectionHeader()}
       {this.renderConnectionIndicator()}
+      <hr />
+      {this.renderTunnelList()}
     </div>
   }
 }
