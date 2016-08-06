98c785ec7ae40614458a05ef6b872cf1d5fe4dc5
niels <nm@9la.dk>
2016-08-06 17:02:16 +0200

    Modify main app background to be dark gray.

--- app/containers/App.js
+++ app/containers/App.js
@@ -5,6 +5,14 @@ import {startTunnel, stopTunnel} from "../actions/tunnelsActions";
 import TunnelList from '../components/TunnelList/TunnelList';
 import ConnectionHeader from '../components/ConnectionHeader/ConnectionHeader';
 
+const style = {
+  root: {
+    backgroundColor: '#b4b6c2',
+    height: '100%',
+    width: '100%'
+  }
+}
+
 class App extends Component {
   onHeaderConnectClick() {
     const connection = this.props.connection.toJS()
@@ -21,12 +29,11 @@ class App extends Component {
 
   render() {
     return (
-      <div>
+      <div style={style.root}>
         <ConnectionHeader
           connection={this.props.connection}
           onConnectClick={this.onHeaderConnectClick.bind(this)}
         />
-        <hr />
         <TunnelList
           tunnels={this.props.tunnels}
           onEnableTunnelClick={this.onEnableTunnelClick.bind(this)}
