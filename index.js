7dc0b2b0535e0e2ab809b39ee356fa359883b468
niels <nm@9la.dk>
2016-08-07 22:25:21 +0200

    Adjust default window size

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 650, height: 400})
 
   // and load the index.html of the app.
   win.loadURL(`file://${__dirname}/index.html`)
