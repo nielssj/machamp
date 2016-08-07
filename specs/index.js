0d1ed37a021c3d4d5ba4d029631451c5fcc54085
niels <nm@9la.dk>
2016-08-07 18:40:33 +0200

    Import font-awesome for icons

--- specs/index.js
+++ specs/index.js
@@ -1,3 +1,8 @@
 import ConnectionHeader from "../app/components/ConnectionHeader/ConnectionHeader.spec"
 import TunnelListSpec from "../app/components/TunnelList/TunnelList.spec"
 import TunnelRowSpec from "../app/components/TunnelRow/TunnelRow.spec"
+
+var node = document.createElement('link');
+node.setAttribute('href', 'css/font-awesome.css')
+node.setAttribute('rel', 'stylesheet')
+document.head.appendChild(node);
